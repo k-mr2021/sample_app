e06fd2838aee4799c8ea5cdfaebed0b686b6d3ff
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-14 07:56:48 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,6 +26,12 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
   private
   # ストロングパラメータ
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
   # .../lists/1や.../lists/3に該当する, asオプションでルーティングに名前を付ける
   get 'lists/:id' => 'lists#show', as: 'list'
   
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
