af76704d157df68f5548ca65857318b7d732b2c3
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-14 02:13:43 +0000

    [Start] 3-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/show'
+  # .../lists/1や.../lists/3に該当する
+  get 'lists/:id' => 'lists#show'
   get 'lists/edit'
  
   
