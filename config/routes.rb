13c996aaef2a4e63c784823997aabbf2896c5f4b
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-15 01:23:30 +0000

    [Finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  get 'lists/new'
   
   get '/top' => 'homes#top'
   
-  post 'lists' => 'lists#create'
-  
-  get 'lists' => 'lists#index'
-  
-  # .../lists/1や.../lists/3に該当する, asオプションでルーティングに名前を付ける
-  get 'lists/:id' => 'lists#show', as: 'list'
-  
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
-  delete 'lists/:id' => 'lists#destroy',as: 'destroy_list'
- 
+  resources :lists
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
