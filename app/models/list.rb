3ea55216c21b67be947af6add8a05f771d1e9a6d
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-15 09:04:26 +0000

    [Start] 3-10

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,6 @@ class List < ApplicationRecord
   
   validates :title, presence: true
   validates :body, presence: true
-  validates :image, presence: true
+  
+  # validates :image, presence: true コメントアウト
 end
