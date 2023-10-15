8c7d5b5048f433b9765c0b0f4019cd2ca5196550
k-mr2021 <143944891+k-mr2021@users.noreply.github.com>
2023-10-15 00:20:27 +0000

    [Start] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   
   patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy',as: 'destroy_list'
  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
